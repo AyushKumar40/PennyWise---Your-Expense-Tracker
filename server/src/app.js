aa5604a597739669e49cbe67043d00bfc4cf86d2
Ayush <ayukumar456@gmail.com>
2026-06-08 01:36:30 +0530

    deployment tweak

--- server/src/app.js
+++ server/src/app.js
@@ -10,10 +10,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin:
-      process.env.NODE_ENV === "production"
-        ? process.env.FRONTEND_URL || "*"
-        : "http://localhost:5173",
+    origin: ["pennywiseee.netlify.app", "http://localhost:5173"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type"],
   }),
